072b434b4ad7d71a7b62f0f3c6bcd77139f3aec8
miha <raven29_89@mail.ru>
2016-09-23 11:41:32 +0300

    add page profile.php

---
+++ views/site/profile.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ */
+
+
+
+$this->title = Yii::t('app', 'The people you have invited');
+?>
+<div class="site-invited-tree">
+    <div class="row">
+        <div class="col-sm-6 center-block" style="float: none;">
+                <div class="panel panel-white border-top-green">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">User Info</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="body-section">
+                            <h5 class="section-heading">Name</h5>
+                            <p class="section-content">Jane A. Doe</p>
+                        </div>
+                        <div class="body-section">
+                            <h5 class="section-heading">Address</h5>
+                            <p class="section-content">46 Gray's Inn Rd, London, WC1X 8LP</p>
+                        </div>
+                        <div class="body-section">
+                            <h5 class="section-heading">Telephone</h5>
+                            <p class="section-content">(123) 456 - 7890</p>
+                        </div>
+                        <div class="body-section">
+                            <h5 class="section-heading">Email</h5>
+                            <p class="section-content">spam@ztapps.com</p>
+                        </div>
+                        <div class="body-section">
+                            <h5 class="section-heading">Website</h5>
+                            <p class="section-content">ztapps.com</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+</div>
