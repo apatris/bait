217e45efaf88a8fb939497833058b225fe99c599
DatsivStepan <datsivStepan@gmail.com>
2019-12-12 15:29:27 +0200

    fix

--- protected/nodejs/parser.js
+++ protected/nodejs/parser.js
@@ -24,6 +24,12 @@ exports.parseSite = async (login, pass, res) => {
 
 	browser.close();
 	res.statusCode = 200;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
 res.setHeader('Content-Type', 'application/json');
 const responseBody = { a: text};
 
