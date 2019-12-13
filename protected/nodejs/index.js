b44bf863c4adc35dc13e534e209e2e47ad58a6c2
DatsivStepan <datsivStepan@gmail.com>
2019-12-13 17:19:18 +0200

    ref code and add email login

--- protected/nodejs/index.js
+++ protected/nodejs/index.js
@@ -8,8 +8,8 @@ http.createServer(function(req, res) {
 
 	switch (urlParsed.pathname) {
 		case '/get-parse-data':
-			if (query && query.login && query.pass) {
-				parser.parseWniski(query.login, query.pass, res).then(result => {
+			if (query && query.login && query.pass && query.email) {
+				parser.parseWniski(query.login, query.pass, query.email).then(result => {
 					res.statusCode = 200;
 				  res.setHeader('Access-Control-Allow-Origin', '*');
 				  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
--- protected/nodejs/parser.js
+++ protected/nodejs/parser.js
@@ -1,49 +1,63 @@
 const puppeteer = require('puppeteer');
 
-exports.parseWniski = async (login, pass) => {
+exports.parseWniski = async (login, pass, email) => {
 	//try {
-		const browser = await puppeteer.launch({args: ['--no-sandbox', '--proxy-server=socks5://172.104.135.13:9050']});
-		//const browser = await puppeteer.launch({headless: false});
+		//const browser = await puppeteer.launch({args: ['--no-sandbox', '--proxy-server=socks5://172.104.135.13:9050']});
+		const browser = await puppeteer.launch({headless: false});
 		const page = await browser.newPage();
 
 		await page.goto('https://wnioski.mazowieckie.pl/MuwWsc/PL');
 		await page.waitFor(1000);
 		const navigationPromise = page.waitForNavigation({waitUntil: ['networkidle2'] })
 
+		//login form begin
 		await page.waitForSelector('[name="UserName"]')
 	  await page.type('[name="UserName"]', login);
-
-		await page.waitForSelector('[name="Password"]')
 	  await page.type('[name="Password"]', pass)
 	  await page.click('#btnSubmitLogin');
+		//login form end
 
 		await page.waitForResponse(response => response.status() === 200);
 		await navigationPromise;
 
+		//check errors after login
 		var item = await page. $('.validation-summary-errors');
 		if (item) {
 			browser.close();
 			return {status:'parse_noauth>>>'};
 		}
+		//
 
+		//email insert form begin
+		var emailE = await page. $('#login-box [name="Email"]');
+		if (emailE) {
+			await page.type('[name="Email"]', email);
+			await page.click('#btnSubmitLogin');
+			await page.waitForResponse(response => response.status() === 200);
+		}
+		//email insert form end
+
+		await page.waitForSelector('.formSection')
 		const tds = await page.$$eval('.formSection', tds => tds.map((td) => {
-			var text = td.textContent;
+			let text = td.textContent;
       return {title:text.trim(), section:td.getAttribute("id")};
  		}));
 
-		let res2 = '';
 		let header = await page.$("h2.formHeader");
 		let headerText = await page.evaluate(header => header.textContent, header);
 
-		for(const elem of tds) {
-			const element2 = await page.$("." + elem.section);
-			let text2 = '';
-			if (element2) {
-				 text2 = await page.evaluate(element2 => element2.textContent, element2);
+		let resDataTds = [];
+		for(const td of tds) {
+			const td2 = await page.$("." + td.section);
+			let textTd2 = '';
+			if (td2) {
+				 textTd2 = await page.evaluate(td2 => td2.textContent, td2);
 			}
-			res2 = res2 + elem.title + '||' + text2.trim() + '###';
+
+			resDataTds.push(td.title + (textTd2 ? '||' + textTd2.trim() + '###' : ''));
 		}
 		browser.close();
 
-		return {status:'parse_complete>>>' + headerText + '::' + res2};
+
+		return {status:'parse_complete>>>' + headerText + '::' + resDataTds.join('###')};
 };
