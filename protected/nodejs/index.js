90148b310abfd38db0af3f94e1091d7b24827dad
DatsivStepan <datsivStepan@gmail.com>
2019-12-16 11:32:40 +0200

    fix

--- protected/nodejs/index.js
+++ protected/nodejs/index.js
@@ -6,12 +6,7 @@ app.get('/get-parse-data', function (req, res) {
 	var query = req.query;
 	if (query && query.login && query.pass && query.email) {
 		parser.parseWniski(query.login, query.pass, query.email).then(result => {
-			res.statusCode = 200;
 			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-			res.setHeader('Access-Control-Allow-Credentials', true);
 			res.setHeader('Content-Type', 'application/json');
 
 			res.write(JSON.stringify(result));
