a8a669ba9ec8547bb8c624f0ac25178da7d02698
DatsivStepan <datsivStepan@gmail.com>
2019-12-23 16:18:49 +0200

    fix

--- protected/nodejs/parser.js
+++ protected/nodejs/parser.js
@@ -119,9 +119,9 @@ exports.parseSantander = async (login, pass) => {
 //	await page.click('table span.show-all-operations.operations-shown');
 
 //	await page.waitForSelector('div.pfm-history-container table tbody tr.transaction-details-row');
-	let urlRR = __dirname + '\\tmp';
+	let urlRR = __dirname + '/tmp';
 	console.log(urlRR);
-	
+
 	await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: urlRR});
 
 	await page.click('#btn-csv');
