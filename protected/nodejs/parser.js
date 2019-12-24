aa7d9a10557151889b49e0128e18788a2668e016
DatsivStepan <datsivStepan@gmail.com>
2019-12-24 12:36:29 +0200

    fix

--- protected/nodejs/parser.js
+++ protected/nodejs/parser.js
@@ -5,9 +5,11 @@ exports.parseSantander = async (login, pass, flag) => {
 	let urlRequest = 'https://e.apatris.pl/mod/api/request-sms?token=bank-token';
 
 	const browser = await puppeteer.launch({args: ['--no-sandbox', '--proxy-server=socks5://172.104.135.13:9050'], userDataDir: './data/data_' + login});
-	 // const browser = await puppeteer.launch({ headless: false, userDataDir: './data/data_' + login});
+	//const browser = await puppeteer.launch({ headless: false, userDataDir: './data/data_' + login});
 
 	const page = await browser.newPage();
+		console.log('go to');
+
 	await page.goto('https://www.centrum24.pl/centrum24-web/login');
 	await page.waitFor(1000);
 	const navigationPromise = page.waitForNavigation({waitUntil: ['networkidle2'] })
