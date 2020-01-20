42c4ecb1a908b2b8b70a9adaa106780e92d36b4b
DatsivStepan <datsivStepan@gmail.com>
2020-01-20 18:00:58 +0200

    fix

--- protected/nodejs/index.js
+++ protected/nodejs/index.js
@@ -10,6 +10,7 @@ cron.schedule('* * * * *', async function () {
 	var date = new Date();
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
+	console.log(minutes)
 	if ((hours == 7) && (minutes == 1)) {
 
 		fs.writeFile('test.txt', '', function (err) { if (err) throw err; });
